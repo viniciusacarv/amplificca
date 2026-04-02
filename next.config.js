e6e71e81f9b8d000d1b72b316461c868d943dbec
viniciusacarv <viniciusdfortal@gmail.com>
2026-04-01 22:45:54 -0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
   images: {
     domains: ['arbxtwjsdxjypgjeoxqr.supabase.co'],
+    unoptimized: true,
   },
 }
 module.exports = nextConfig
