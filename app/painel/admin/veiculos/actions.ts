9787f65dde3596426b854c885141ff0cf4f49b21
viniciusacarv <viniciusdfortal@gmail.com>
2026-04-23 09:15:08 -0300

    Update actions.ts

--- app/painel/admin/veiculos/actions.ts
+++ app/painel/admin/veiculos/actions.ts
@@ -1,7 +1,11 @@
 'use server'
 
-import { salvarVeiculo as salvarVeiculoAdminImprensa } from '../imprensa/actions'
+import { salvarVeiculo as salvarVeiculoAdminImprensa, excluirVeiculo as excluirVeiculoAdminImprensa } from '../imprensa/actions'
 
 export async function salvarVeiculo(formData: FormData) {
   return salvarVeiculoAdminImprensa(formData)
+}
+
+export async function excluirVeiculo(formData: FormData) {
+  return excluirVeiculoAdminImprensa(formData)
 }
